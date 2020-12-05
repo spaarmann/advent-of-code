bc44fb6626f642c50eb893d3d6a72bcff86c2022
Sebastian Paarmann <git@s-paarmann.de>
2020-12-05 12:52:28 +0100

    [2020-day5] Slightly cleverer parsing

--- 2020/src/day5.rs
+++ 2020/src/day5.rs
@@ -1,35 +1,23 @@
-fn parse(input: &str) -> impl Iterator<Item = (u64, u64, u64)> + '_ {
-    input.lines().map(|line| {
-        let mut chars = line.chars();
-        let mut row_range = (0, 128); // [0, 128)
+fn to_digit(c: char) -> u64 {
+    match c {
+        'F' | 'L' => 0,
+        'B' | 'R' => 1,
+        _ => panic!("invalid input"),
+    }
+}
 
-        for _ in 0..7 {
-            match chars.next().unwrap() {
-                'F' => row_range.1 -= (row_range.1 - row_range.0) >> 1,
-                'B' => row_range.0 += (row_range.1 - row_range.0) >> 1,
-                _ => panic!("invalid input"),
-            }
-        }
-
-        let mut col_range = (0, 8);
-        for _ in 0..3 {
-            match chars.next().unwrap() {
-                'L' => col_range.1 -= (col_range.1 - col_range.0) >> 1,
-                'R' => col_range.0 += (col_range.1 - col_range.0) >> 1,
-                _ => panic!("invlid input"),
-            }
-        }
-
-        (row_range.0, col_range.0, row_range.0 * 8 + col_range.0)
-    })
+fn parse(input: &str) -> impl Iterator<Item = u64> + '_ {
+    input
+        .lines()
+        .map(|line| line.chars().fold(0, |v, c| (v << 1) | to_digit(c)))
 }
 
 pub fn part1(input: &str) -> u64 {
-    parse(input).map(|seat| seat.2).max().unwrap()
+    parse(input).max().unwrap()
 }
 
 pub fn part2(input: &str) -> u64 {
-    let mut seats = parse(input).map(|seat| seat.2).collect::<Vec<_>>();
+    let mut seats = parse(input).collect::<Vec<_>>();
     seats.sort_unstable();
     for i in 1..seats.len() {
         if seats[i - 1] + 2 == seats[i] {
@@ -58,10 +46,10 @@ mod tests {
     #[test]
     fn p1_example1() {
         let seats = parse(EXAMPLE1_INPUT).collect::<Vec<_>>();
-        assert_eq!(seats[0], (44, 5, 357));
-        assert_eq!(seats[1], (70, 7, 567));
-        assert_eq!(seats[2], (14, 7, 119));
-        assert_eq!(seats[3], (102, 4, 820));
+        assert_eq!(seats[0], 357);
+        assert_eq!(seats[1], 567);
+        assert_eq!(seats[2], 119);
+        assert_eq!(seats[3], 820);
         assert_eq!(part1(EXAMPLE1_INPUT), 820);
     }
 
