5b0ba33d96df1fa09baa0641fab2fdaa7c17976e
Sebastian Paarmann <git@s-paarmann.de>
2021-12-07 09:14:50 +0100

    Clean up day 7 a bit

--- 2021/src/day7.rs
+++ 2021/src/day7.rs
@@ -1,4 +1,4 @@
-pub fn part1(input: &str) -> u64 {
+fn find_min_cost<F: Fn(u64) -> u64>(input: &str, cost_from_distance: F) -> u64 {
     let numbers: Vec<u64> = input
         .split(',')
         .map(|n| n.trim().parse().unwrap())
@@ -9,30 +9,19 @@ pub fn part1(input: &str) -> u64 {
 
     for n in numbers {
         for p in 0..costs.len() {
-            costs[p] += (n as i64 - p as i64).abs();
+            costs[p] += cost_from_distance((n as i64 - p as i64).unsigned_abs());
         }
     }
 
-    *costs.iter().min().unwrap() as u64
+    *costs.iter().min().unwrap()
+}
+
+pub fn part1(input: &str) -> u64 {
+    find_min_cost(input, |dist| dist)
 }
 
 pub fn part2(input: &str) -> u64 {
-    let numbers: Vec<u64> = input
-        .split(',')
-        .map(|n| n.trim().parse().unwrap())
-        .collect();
-    let max = *numbers.iter().max().unwrap();
-
-    let mut costs = vec![0; (max + 1) as usize];
-
-    for n in numbers {
-        for p in 0..costs.len() {
-            let d = (n as i64 - p as i64).abs();
-            costs[p] += (d * (d + 1)) / 2;
-        }
-    }
-
-    *costs.iter().min().unwrap() as u64
+    find_min_cost(input, |dist| (dist * (dist + 1) / 2))
 }
 
 #[cfg(test)]
