7d41270f019c8bb9bee5c9e75418445eddf2fbc9
Sebastian Paarmann <git@s-paarmann.de>
2023-12-13 20:11:43 +0100

    2023 Day 13 Part 2

--- 2023/src/day13.rs
+++ 2023/src/day13.rs
@@ -2,35 +2,45 @@ use itertools::Itertools;
 
 use crate::util::Grid;
 
-fn find_reflection(xss: Vec<Vec<char>>) -> Option<u64> {
+fn count_neq(xs: &[char], ys: &[char]) -> usize {
+    std::iter::zip(xs, ys).filter(|&(&x, &y)| x != y).count()
+}
+
+fn find_reflection(xss: Vec<Vec<char>>, smudge: usize) -> Option<u64> {
     'xss: for i in 0..xss.len() - 1 {
-        if xss[i] == xss[i + 1] {
+        let mut smudge_remaining = smudge as i64;
+
+        smudge_remaining -= count_neq(&xss[i], &xss[i + 1]) as i64;
+        if smudge_remaining >= 0 {
             let dist_to_edge = usize::min(i, xss.len() - 1 - (i + 1));
             for j in 1..=dist_to_edge {
-                if xss[i - j] != xss[i + 1 + j] {
+                smudge_remaining -= count_neq(&xss[i - j], &xss[i + 1 + j]) as i64;
+                if smudge_remaining < 0 {
                     continue 'xss;
                 }
             }
 
-            return Some((i + 1) as u64);
+            if smudge_remaining == 0 {
+                return Some((i + 1) as u64);
+            }
         }
     }
 
     None
 }
 
-pub fn part1(input: &str) -> u64 {
+fn solve(input: &str, smudge: usize) -> u64 {
     let mut sum = 0;
 
     for pattern in input.split("\n\n") {
         let grid = Grid::<char>::from_char_grid(pattern);
 
         let rows = grid.rows().map(|r| r.copied().collect_vec()).collect_vec();
-        if let Some(n) = find_reflection(rows) {
+        if let Some(n) = find_reflection(rows, smudge) {
             sum += 100 * n;
         } else {
             let cols = grid.cols().map(|c| c.copied().collect_vec()).collect_vec();
-            if let Some(n) = find_reflection(cols) {
+            if let Some(n) = find_reflection(cols, smudge) {
                 sum += n;
             }
         }
@@ -39,8 +49,12 @@ pub fn part1(input: &str) -> u64 {
     sum
 }
 
+pub fn part1(input: &str) -> u64 {
+    solve(input, 0)
+}
+
 pub fn part2(input: &str) -> u64 {
-    todo!()
+    solve(input, 1)
 }
 
 #[cfg(test)]
@@ -83,6 +97,6 @@ mod tests {
     #[test]
     fn p2_input() {
         let input = std::fs::read_to_string("input/day13").expect("reading input file");
-        assert_eq!(part2(&input), todo!());
+        assert_eq!(part2(&input), 39037);
     }
 }
