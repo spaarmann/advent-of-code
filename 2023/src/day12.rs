36e556f05845c732e21c2cfbe1a62b940bb01fc3
Sebastian Paarmann <git@s-paarmann.de>
2023-12-13 20:41:04 +0100

    2023 Day 12 Part 1

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -40,6 +40,7 @@ fn solve(
         if group_pos == groups.len() && curr_group_len == 0 {
             return 1;
         }
+        return 0;
     }
 
     match springs[pos] {
@@ -53,22 +54,41 @@ fn solve(
             }
         }
         Spring::Damaged => {
-            if groups[group_pos] > curr_group_len + 1 {
+            if group_pos == groups.len() {
+                0
+            } else if curr_group_len + 1 > groups[group_pos] {
                 0
             } else {
                 solve(springs, groups, pos + 1, group_pos, curr_group_len + 1)
             }
         }
-        Spring::Unknown => todo!(),
+        Spring::Unknown => {
+            springs[pos] = Spring::Damaged;
+            let solutions_damaged = solve(springs, groups, pos, group_pos, curr_group_len);
+            springs[pos] = Spring::Operational;
+            let solutions_operational = solve(springs, groups, pos, group_pos, curr_group_len);
+            springs[pos] = Spring::Unknown;
+            solutions_damaged + solutions_operational
+        }
     }
 }
 
 pub fn part1(input: &str) -> u64 {
-    todo!()
+    parse(input)
+        .map(|(mut springs, groups)| solve(&mut springs, &groups, 0, 0, 0))
+        .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
-    todo!()
+    parse(input)
+        .map(|(mut springs, mut groups)| {
+            let slen = springs.len();
+            springs = springs.into_iter().cycle().take(5 * slen).collect_vec();
+            let glen = groups.len();
+            groups = groups.into_iter().cycle().take(5 * glen).collect_vec();
+            solve(&mut springs, &groups, 0, 0, 0)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -91,12 +111,12 @@ mod tests {
     #[test]
     fn p1_input() {
         let input = std::fs::read_to_string("input/day12").expect("reading input file");
-        assert_eq!(part1(&input), todo!());
+        assert_eq!(part1(&input), 7084);
     }
 
     #[test]
     fn p2_example() {
-        assert_eq!(part2(EXAMPLE_INPUT), todo!());
+        assert_eq!(part2(EXAMPLE_INPUT), 525152);
     }
 
     #[test]
