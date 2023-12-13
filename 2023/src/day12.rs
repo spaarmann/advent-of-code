c4138404bc0349acade547eaed136e773ea4ddf9
Sebastian Paarmann <git@s-paarmann.de>
2023-12-13 21:06:57 +0100

    2023 Day 12 Horrible memoization to make part 2 work

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -1,8 +1,12 @@
 use itertools::Itertools;
+use std::{
+    collections::{HashMap, VecDeque},
+    iter::once,
+};
 
 use crate::util::SplitAndParse;
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 enum Spring {
     Unknown,
     Operational,
@@ -26,48 +30,76 @@ fn parse(input: &str) -> impl Iterator<Item = (Vec<Spring>, Vec<usize>)> + '_ {
     })
 }
 
+type MemoStore = HashMap<(VecDeque<Spring>, VecDeque<usize>, usize), u64>;
+
 fn solve(
-    springs: &mut [Spring],
-    groups: &[usize],
-    pos: usize,
-    group_pos: usize,
+    memo: &mut MemoStore,
+    springs: &mut VecDeque<Spring>,
+    groups: &mut VecDeque<usize>,
     curr_group_len: usize,
 ) -> u64 {
-    if pos == springs.len() {
-        if group_pos == groups.len() - 1 && curr_group_len == groups[group_pos] {
+    if let Some(res) = memo.get(&(springs.clone(), groups.clone(), curr_group_len)) {
+        return *res;
+    }
+
+    let res = solve_algo(memo, springs, groups, curr_group_len);
+    memo.insert((springs.clone(), groups.clone(), curr_group_len), res);
+    return res;
+}
+
+fn solve_algo(
+    memo: &mut MemoStore,
+    springs: &mut VecDeque<Spring>,
+    groups: &mut VecDeque<usize>,
+    curr_group_len: usize,
+) -> u64 {
+    if springs.is_empty() {
+        if groups.is_empty() || groups.len() == 1 && groups[0] == curr_group_len {
             return 1;
         }
-        if group_pos == groups.len() && curr_group_len == 0 {
-            return 1;
-        }
+
         return 0;
     }
 
-    match springs[pos] {
+    match springs[0] {
         Spring::Operational => {
             if curr_group_len == 0 {
-                solve(springs, groups, pos + 1, group_pos, 0)
-            } else if curr_group_len == groups[group_pos] {
-                solve(springs, groups, pos + 1, group_pos + 1, 0)
+                springs.pop_front();
+                let res = solve(memo, springs, groups, 0);
+                springs.push_front(Spring::Operational);
+                return res;
+            } else if curr_group_len == groups[0] {
+                springs.pop_front();
+                let grp = groups.pop_front().unwrap();
+                let res = solve(memo, springs, groups, 0);
+                groups.push_front(grp);
+                springs.push_front(Spring::Operational);
+                return res;
             } else {
                 0
             }
         }
         Spring::Damaged => {
-            if group_pos == groups.len() {
+            if groups.len() == 0 {
                 0
-            } else if curr_group_len + 1 > groups[group_pos] {
+            } else if curr_group_len + 1 > groups[0] {
                 0
             } else {
-                solve(springs, groups, pos + 1, group_pos, curr_group_len + 1)
+                springs.pop_front();
+                let res = solve(memo, springs, groups, curr_group_len + 1);
+                springs.push_front(Spring::Damaged);
+                return res;
             }
         }
         Spring::Unknown => {
-            springs[pos] = Spring::Damaged;
-            let solutions_damaged = solve(springs, groups, pos, group_pos, curr_group_len);
-            springs[pos] = Spring::Operational;
-            let solutions_operational = solve(springs, groups, pos, group_pos, curr_group_len);
-            springs[pos] = Spring::Unknown;
+            springs.pop_front();
+            springs.push_front(Spring::Damaged);
+            let solutions_damaged = solve(memo, springs, groups, curr_group_len);
+            springs.pop_front();
+            springs.push_front(Spring::Operational);
+            let solutions_operational = solve(memo, springs, groups, curr_group_len);
+            springs.pop_front();
+            springs.push_front(Spring::Unknown);
             solutions_damaged + solutions_operational
         }
     }
@@ -75,18 +107,30 @@ fn solve(
 
 pub fn part1(input: &str) -> u64 {
     parse(input)
-        .map(|(mut springs, groups)| solve(&mut springs, &groups, 0, 0, 0))
+        .map(|(springs, groups)| {
+            solve(
+                &mut MemoStore::new(),
+                &mut springs.into(),
+                &mut groups.into(),
+                0,
+            )
+        })
         .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
     parse(input)
-        .map(|(mut springs, mut groups)| {
+        .map(|(springs, groups)| {
             let slen = springs.len();
-            springs = springs.into_iter().cycle().take(5 * slen).collect_vec();
+            let mut springs = once(Spring::Unknown)
+                .chain(springs.into_iter())
+                .cycle()
+                .skip(1)
+                .take(5 * slen + 4)
+                .collect();
             let glen = groups.len();
-            groups = groups.into_iter().cycle().take(5 * glen).collect_vec();
-            solve(&mut springs, &groups, 0, 0, 0)
+            let mut groups = groups.into_iter().cycle().take(5 * glen).collect();
+            solve(&mut MemoStore::new(), &mut springs, &mut groups, 0)
         })
         .sum()
 }
@@ -119,9 +163,11 @@ mod tests {
         assert_eq!(part2(EXAMPLE_INPUT), 525152);
     }
 
+    // This is too slow in debug mode, because the memoization is stupidly inefficient.
+    #[cfg(not(debug_assertions))]
     #[test]
     fn p2_input() {
         let input = std::fs::read_to_string("input/day12").expect("reading input file");
-        assert_eq!(part2(&input), todo!());
+        assert_eq!(part2(&input), 8414003326821);
     }
 }
